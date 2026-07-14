b508632844e8c897a8c40c2d65f003b780acbcf4
Desire-e <desireeruedatorresalmeria@gmail.com>
2026-07-14 15:18:48 +0200

    new footer

--- src/components/app/Footer.jsx
+++ src/components/app/Footer.jsx
@@ -1,30 +1,61 @@
 function Footer() {
     return (
         <footer>
-            <div className="footer-container">
+            <div className="footer-top">
+                
+                {/* TODO. RESPONSIVE */}
                 <div className="footer-item">
-                    <h6>Contacto</h6>
-                    <ul>
-                        <li>Correo · desireeruedatorresalmeria@gmail.com</li>
-                        <li>LinkedIn · Desirée Rueda Torres</li>
-                        <li>GitHub · desire-e</li>
-                        <li>Telefono · +34 000 00 00 00</li>
-                    </ul>
+                    <img src={`${import.meta.env.BASE_URL}images/logotipo.png`} 
+                    className="logotipo" alt="Logotipo" width={"250px"}/>
+                    
+                    <p>Desarrolladora web junior en formación, flexible y con entusiasmo de seguir aprendiendo.</p>
                 </div>
+
+
+                <div className="separator"></div>
+
+
+                <div className="footer-item">
+                    {/* TODO. RESPONSIVE */}
+                    <ul>
+                        <h5>Contacto</h5>
+                        <li> <i class="bi bi-envelope"></i> desireeruedatorresalmeria@gmail.com </li>
+                        <li> <i class="bi bi-telephone"></i> +34 605726030 </li>
+                    </ul>
+
+                    {/* TODO. RESPONSIVE */}
+                    <div className="profiles">
+                        <h5>Perfiles</h5>
+
+                        <a href=""><i class="bi bi-github"></i></a>
+                        
+                        <a href=""><i class="bi bi-linkedin"></i></a>
+                        
+                        <a href="">
+                            <img src={`${import.meta.env.BASE_URL}images/europass-logo.png`} 
+                            className="" alt="Logotipo" width={"100px"}/>
+                        </a>
+                    </div>
+
+                </div>
+
                 
                 <div className="separator"></div>
 
+                {/* TODO. RESPONSIVE */}
                 <div className="footer-item">
-                    <h6>Disponibilidad</h6>
-                    
                     <ul>
+                        <h5>Disponibilidad</h5>
                         <li>Buscando primera oportunidad profesional / prácticas</li>
                         <li>Disponible para remoto o híbrido</li>
                     </ul>
                 </div>
+
             </div>
-            
-            <p>© 2026 Desirée Rueda Torres</p>
+
+            <div className="footer-bottom">
+                <p>© 2026 Desirée Rueda Torres</p>
+            </div>
         </footer>
     );
 };
